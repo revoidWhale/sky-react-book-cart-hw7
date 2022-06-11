31b4f3cc5bfee51b8a0f623f781d8b9cd9b289c2
Evgeniy Eremin <103576500+revoidWhale@users.noreply.github.com>
2022-06-11 13:39:24 +0700

    Delete src directory

--- src/components/CartTotal.js
+++
@@ -1,7 +0,0 @@
-function CartTotal({ books }) {
-  const total = books.reduce((sum, pr) => sum + pr.price * pr.quantity, 0)
-  console.log(books)
-  return <p>{total}</p>
-}
-
-export default CartTotal
